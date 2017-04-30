ae5595d1140ae4908419510b6aa0bded47921d5c
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-04-30 15:08:35 +1000

    more auth with oauth2client

--- auth_with_apiclient.py
+++ auth_with_apiclient.py
@@ -7,6 +7,7 @@ from oauth2client import client, file
 class auth_with_apiclient.py:
     """
     My implementation of using the google-api-client module to create an authorized service object, which can make oauth2.0 authorized Google API calls.
+    ** It *should* return an google OAuth2Credentials object.   **
     """
     def __init__(self, client_path=None, scope=None):
         if client_path:
@@ -37,6 +38,7 @@ class auth_with_apiclient.py:
     drive_service = build('drive', 'v2', http=http_auth)
 
     # we need to store.credentials.to_json() in a file.
+    #   WHY NOT PICKLE THE CREDENTIALS??
     token_path = $PATH
     import json
     json.dump(open(token_path, 'w'), credentials.to_json())
@@ -49,6 +51,18 @@ class auth_with_apiclient.py:
     file_Storage_object.put(credentials)
     # release lock:
     file_Storage_object.release_lock()
+    # After doing some reading, the google docs state that credential objects can be safely pickled and unpickled.. I think that this would be better than using a "Storage" object, as the Storage object stores the tokens in a human readable file, whereas pickle stores the object in a non-human-readable binary file:
+    token_path = self.tokens
+    # or
+    flow = pickle.load( open(self.tokens, 'rb')
+    # you don't even have to store the tokens as a seperate object, but this might be better because if the tokens are class attributes, they will be visible with: self.tokens - for example..
+    # How's about pickling an OAuth2Credentials object, or flow (whatever):
+    import pickle
+    token_path = self.token_path
+    pickle.dump( self, open(token_path, 'wb') )
 
     # TODO:
     #   - write methods to retrieve credentials from file.
+    #   - the docs advise that if the redirect_uri serves html, then as we know, the authorization code is visible in the URL. We can prevent this by intercepting the auth_code, then redirecting the browser to another page..
+    # HOW DO WE REDIRECT THE PAGE WITH PYTHON???
+    #   - I was thinking how we could hide the client id and secret using pickle??? Maybe we could pickle the ClientCredentials object, then retrieve it by unpickling it??
