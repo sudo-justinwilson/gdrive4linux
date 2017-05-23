2b19361eadc094d33e7bfae3d999ba24d9ded37d
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-05-23 12:18:49 +1000

    created working pickled_service2_without_pickle_state_methods.py

--- pickled_service2_without_pickle_state_methods.py
+++ pickled_service2_without_pickle_state_methods.py
@@ -31,33 +31,10 @@ class auth_with_apiclient:
         else:
             self.pickled = False
 
-    ### start pickle
-    #def __getstate__(self):
-    #    """
-    #    Copy the instance's state for pickling.
-    #    Method called by pickle module.
-    #    """
-    #    state = self.__dict__.copy()
-    #    return state
-
-    #def __setstate__(self, state):
-    #    """
-    #    This is a method to restore the state of this instance.
-    #    Called by pickle module.
-    #    """
-    #    self.__dict__.update(state)
-
-    ### end pickle
-
     def create_service(self, auth_code=None, url=None, http_auth=None):
         """
         Method that returns authenticated service object.
         """
-        #if not url:
-        #    url = self.auth_uri
-        #if not auth_code:
-        #    auth_code = self.get_auth_code(url)
-        #if not os.path.exists(self.pickle_path):
         if self.pickled is False:
             if not url:
                 url = self.auth_uri
@@ -66,9 +43,6 @@ class auth_with_apiclient:
             self.credentials = self.flow.step2_exchange(auth_code)
             self.pickled = True
             pickle.dump( self, open(self.pickle_path, 'wb') )
-        #else:
-        #    instance = pickle.load( open(self.pickle_path, 'rb') )
-        #    self.credentials = instance.credentials
         if not http_auth:
             http_auth = self.credentials.authorize(httplib2.Http())
         drive_service = build('drive', 'v2', http=http_auth)
@@ -78,7 +52,6 @@ class auth_with_apiclient:
         webbrowser.open_new(url)
         val = None
         print('the val is: ', val)
-        #val = webserver.catch_response()
         val = self.webserver.catch_response()
         print('the val after calling webserver.catch_response() is: ', val)
         return val
@@ -93,12 +66,17 @@ if __name__ == '__main__':
     print('the pickle flag after create_service, is set to: ', instance.pickled)
     print(dir(service))
     print(type(service))
-    print(dir(instance.credentials))
+    print("Here is the unpickled credentials introspection: ", dir(instance.credentials))
     print(type(instance.credentials))
     print(instance.credentials.to_json())
+    if instance.credentials.access_token_expired:
+        print('the access_token_expired is True')
+    else:
+        print('access_token_expired == FALSE!')
     #unpickled_object = pickle.load( open(instance.pickle_path, 'rb') )
     #print( type(unpickled_object) )
     #print( dir(unpickled_object) )
     #print(' the type of the thing is: ', type(unpickled_object.credentials))
     #print(' the dir of the thing is: ', dir(unpickled_object.credentials))
-    # 2017/05/22: I have managed to pickle and restore an instance, but now I need to add the logic so I know if I've already authorized, and don't have to open up the webbrowser again because we already have the tokens
+    # 2017/05/23: I was curious to see if it worked without the __{g,s}etstate__ methods, and if they had any effect, in this version I removed them, and they seem to work..
+    # 2017/05/23: Yeah, so it turns out that I don't need the __{g,s}etstate__ methods to pickle the instance, with populated credentials..
