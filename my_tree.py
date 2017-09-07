707644215368917313d28a75f512353729e44fe0
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-09-07 22:10:31 +1000

    Created my_tree1.py to explore different tree structures.

--- my_tree.py
+++ my_tree.py
@@ -1,9 +1,76 @@
-from trethsee_ADT import Tree
+from my_tree_ADT import Tree_ADT
 
-class my_tree(Tree):r
+class my_tree(Tree_ADT):
     """ 
    This is my implementation of a general tree structure to store metadata about the google drive files.
     """ 
+    class Node:
+        """
+        A simple data structure that will act as nodes in the tree.
+        """
+        __slots__ = 'parent', 'children', 'element'
+
+        def __init__(self, parent, element):
+            """
+            Initialize a simple Node instance
+            """
+            self.parent = parent
+            self.element = element
+            self.children = None
+
+    ##  End nested Node class       ##
+    ##  Begin nested file class     ##
+    class File:
+        """
+        This is a data structure where I can store the metadata of a file.
+        """
+        __slots__ == 'file_id', 'title', 'mime_type', 'md5', 'parent' 
+        def __init__(self, file_id, title, mime_type, parent, md5 = None):
+            """
+            Initialize a File object with provided metadata.
+            """
+            self.file_id = file_id
+            self.title = title
+            self.mime_type = mime_type
+            self.parent = parent
+            self.md5 = md5
+
+    ##  End nested file class       ##
+    ##  S
+    ##  End nested classes           ##
+    def __init__(self, root):
+        """
+        Initialize an empty tree.
+        """
+        self.root = self.Node(None, element)
+        self.size = 1
+        self.root.children = dict()
+
+    def _make_path_from_string(self, PATH):
+        """
+        Return the node of the path = PATH.
+        PATH should be a string. IE: "$DRIVE_ROOT_DIR/Books/Calibre/mybook.pdf".
+        The path should be relative from the GDRIVE_ROOT_DIR.
+        Returns a tuple with (Node(parent), str(file_title)).
+        """
+        l = PATH.split('/')
+        current_parent = l.pop(0)
+        while len(l) > 1:
+            current_parent = current_parent.children[l.pop(0)]
+        file_title = current_parent[-1]
+        return (current_parent, file_title)
+
+    def add_child_node(self, n, parent):
+        """
+        Add node 'n' as a child to parent.
+        """
+        parent.children[file_id] = self.Node(parent, element)
+
+
+
+
+
+###     THIS IS THE OLD CODE BELOW:
     def root(self):
         """
         Return the position of the root of the tree, or None if is empty.
---
+++ my_tree_ADT.py
@@ -0,0 +1,57 @@
+class Tree_ADT:
+    """
+    Simple tree structure to be used as the base tree ADT.
+    """
+    #class Node:
+    #    """
+    #    A simple data structure that will act as nodes in the tree.
+    #    """
+    #    __slots__ = 'parent', 'children', 'element'
+    #    def __init__(self, parent, element):
+    #        """
+    #        Initialize a simple Node instance
+    #        """
+    #        self.parent = parent
+    #        self.element = element
+
+    ##  End nested Node class       ##
+    #def __init__(self, root):
+    #    """
+    #    Initialize an empty tree_ADT.
+    #    """
+    #    self.root = self.Node(None, element)
+    #    self.size = 1
+    #    self.root.children = dict()
+
+    def add_child_node(self):
+        """
+        Add a child node to parent.
+        """
+        raise NotImplementedError('Needs to be implemented by subclass')
+
+    def parent(self, node):
+        """
+        Return parent of node.
+        """
+        raise NotImplementedError('Needs to be implemented by subclass')
+
+    def is_root(self, n):
+        """
+        Return True if n is root node.
+        """
+        return self.root() == n
+
+    def is_leaf(self, n):
+        """
+        Return True if n is root node.
+        """
+        return n.children is None
+
+    def __len__(self, node=self.root):
+        raise NotImplementedError('Needs to be implemented by subclass')
+
+    def __iter__(self):
+        """
+        Iterate through the tree's elements.
+        """
+        raise NotImplementedError('Needs to be implemented by subclass')
